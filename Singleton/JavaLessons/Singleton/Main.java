750071bd976138ab2661c425df6428c858a577cc
Panier <32664949+Panier@users.noreply.github.com>
2017-10-22 21:44:27 +0200

    Add files via upload

---
+++ Singleton/JavaLessons/Singleton/Main.java
@@ -0,0 +1,23 @@
+package JavaLessons.Singleton;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SimpleSingleton ss = SimpleSingleton.getInstance();
+		SimpleSingleton ss1 = SimpleSingleton.getInstance();
+		SimpleSingleton ss2 = SimpleSingleton.getInstance();
+		int s1,s2,s3;
+		s1 = ss.hashCode();
+		s2 = ss1.hashCode();
+		s3 = ss2.hashCode();
+		if (s1 == s2 && s2 == s3)
+		{
+			System.out.print("Ta sama referencja");
+		}
+		
+	}
+	
+	
+	
+
+}
---
+++ Singleton/JavaLessons/Singleton/SimpleSingleton.java
@@ -0,0 +1,14 @@
+package JavaLessons.Singleton;
+
+public class SimpleSingleton {
+   
+	private static SimpleSingleton INSTANCE;
+    
+    private SimpleSingleton(){}
+ 
+    public static SimpleSingleton getInstance(){
+        if(INSTANCE==null)
+            INSTANCE = new SimpleSingleton();
+        return INSTANCE;
+    }
+}
